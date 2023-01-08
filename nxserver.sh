62222fe5ebc71c97ad1cb8816f3883bbaabc7d89
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-01-08 01:28:26 +0100

    Update nxserver.sh

--- nxserver.sh
+++ nxserver.sh
@@ -4,5 +4,6 @@ groupadd -r $USER -g 433 \
 && mkdir /home/$USER \
 && chown -R $USER:$USER /home/$USER \
 && echo $USER':'$PASSWORD | chpasswd
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 /etc/NX/nxserver --startup
 tail -f /usr/NX/var/log/nxserver.log
